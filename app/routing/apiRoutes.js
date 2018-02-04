d56e3b16177b414948628639357d37143d045943
Katy <ihatetoast@gmail.com>
2018-02-03 18:16:44 -0600

    added questions.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,26 @@
+// Your apiRoutes.js file should contain two routes:
+
+// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+
+// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// You should save the data in your app as an array of objects. Each of these objects should roughly follow the format below.
+
+/*You should save the data in your app as an array of objects. Each of these objects should roughly follow the format below.
+
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+*/
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,5 @@
+// Your htmlRoutes.js file should include two routes:
+
+// A GET Route to /survey which should display the survey page.
+// A default USE route that leads to index.html which displays the home page.
+
